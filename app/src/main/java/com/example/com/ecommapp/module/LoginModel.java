111913cb67bd94ea7895e679ac88b1b259c52048
rBrilliant <499953902@qq.com>
2018-01-07 16:38:38 +0800

    添加相应的接口

---
+++ app/src/main/java/com/example/com/ecommapp/module/LoginModel.java
@@ -0,0 +1,12 @@
+package com.example.com.ecommapp.module;
+
+/**
+ * Created by rhm on 2018/1/7.
+ */
+
+public class LoginModel extends BaseModel {
+    /**
+     * 用户ID
+     */
+    public String userId;
+}
--- app/src/main/java/com/example/com/ecommapp/network/http/HttpRequest.java
+++ app/src/main/java/com/example/com/ecommapp/network/http/HttpRequest.java
@@ -2,12 +2,17 @@ package com.example.com.ecommapp.network.http;
 
 import android.app.DownloadManager;
 
+import com.example.com.ecommapp.module.BaseModel;
+import com.example.com.ecommapp.module.LoginModel;
 import com.example.com.ecommapp.module.recommand.RecommendModel;
 import com.example.com.ecommapp.module.version.VersionModel;
 import com.example.com.support.okhttp.CommOkhttpClient;
 import com.example.com.support.okhttp.listener.DisposableHandler;
 import com.example.com.support.okhttp.listener.DisposeListener;
 import com.example.com.support.okhttp.request.CommonRequest;
+import com.example.com.support.okhttp.request.RequestParam;
+
+import java.net.HttpURLConnection;
 
 import okhttp3.Request;
 
@@ -31,8 +36,30 @@ public class HttpRequest {
     }
 
     //获取验证码
+    public static void recodeRequest(String phone, DisposeListener listener) {
+        RequestParam requestParam = new RequestParam();
+        requestParam.put("phone", phone);
+        Request request = CommonRequest.getInstance().createGet(HttpConstants.SMSCODE, requestParam);
+        CommOkhttpClient.getInstance().sendRequest(request, new DisposableHandler(BaseModel.class, listener));
+    }
 
     //登录
+    public static void loginRequest(String phone, String password, DisposeListener listener) {
+        RequestParam requestParam = new RequestParam();
+        requestParam.put("phone", phone);
+        requestParam.put("password", password);
+        Request request = CommonRequest.getInstance().createPost(HttpConstants.LOGIN, requestParam);
+        CommOkhttpClient.getInstance().sendRequest(request, new DisposableHandler(LoginModel.class, listener));
+    }
 
     //重置密码
+    public static void passwordRequest(String phone, String smsCode, String password, String newPassword, DisposeListener listener) {
+        RequestParam requestParam = new RequestParam();
+        requestParam.put("phone", phone);
+        requestParam.put("passwrod", password);
+        requestParam.put("smsCode", smsCode);
+        requestParam.put("newPassword", newPassword);
+        Request request = CommonRequest.getInstance().createPost(HttpConstants.PASSWORD, requestParam);
+        CommOkhttpClient.getInstance().sendRequest(request, new DisposableHandler(BaseModel.class, listener));
+    }
 }
